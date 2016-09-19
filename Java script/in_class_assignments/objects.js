5c594d42d960f8ee81e6664132081b08a2fed223
Unknown <addisonbmoyer@gmail.com>
2016-09-19 11:34:52 -0600

    Created In class assignment
    ^^^^

---
+++ Java script/in_class_assignments/objects.js
@@ -0,0 +1,21 @@
+//Objects Lesson
+var Robot = {
+    fuel: 100,
+    weapon: "Laser",
+    speed: 10,
+    strength: 50,
+    armor: "Light",
+
+    AddFuel: function (tank) {
+        fuel += tank;
+    },
+
+    UpgradeArmor: function (change) {
+        armor = change;
+    },
+    ChangeWeapon function (change) {
+        weapon = change;
+    }
+}
+Robot.speed = parseInt(promt("Increase Robot speed?"));
+alert("Our Robot now has a speed of " + Robot.speed);
