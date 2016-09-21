ee85c9170d0f06e311c2f0b311a6f50314030a07
Unknown <addisonbmoyer@gmail.com>
2016-09-21 11:21:25 -0600

    added an alert

--- Java script/in_class_assignments/objectConstructors.js
+++ Java script/in_class_assignments/objectConstructors.js
@@ -7,9 +7,11 @@ function Player() {
 }
 
 var Player1 = new Player();
-Player1.name = prompt("Player1 their leader");
+Player1.name = prompt("Fred their leader");
+alert("Player1's name is now " + Player1.name);
 Player1.health = 8;
 
 var Player2 = new Player();
 Player2.name = prompt("Player2");
+alert("Player2's name is now " + Player2.name);
 Player2.speed =45;
