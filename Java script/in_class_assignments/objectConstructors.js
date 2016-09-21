cbd858c3cdfbc3f8eb67719a9e10e560554de789
Unknown <addisonbmoyer@gmail.com>
2016-09-21 11:56:29 -0600

    done in class

--- Java script/in_class_assignments/objectConstructors.js
+++ Java script/in_class_assignments/objectConstructors.js
@@ -1,17 +1,29 @@
 
 
-function Player() {
+var Player = function () {
     var name;
     var speed = 10;
     var health = 100;
+    var healing = "Strong";
+    this.ammoCount = 100;
+
+    this.reArm = function (ammo) {
+        this.ammoCount += ammo;
+    }
 }
 
 var Player1 = new Player();
-Player1.name = prompt("Fred their leader");
-alert("Player1's name is now " + Player1.name);
-Player1.health = 8;
-
 var Player2 = new Player();
-Player2.name = prompt("Player2");
-alert("Player2's name is now " + Player2.name);
-Player2.speed =45;
+
+
+
+function UpdatePlayer(currentPlayer, healthUpdate, ammo) {
+    currentPlayer.name = prompt("Who is your leader?");
+    alert("Your leader's name is now " + currentPlayer.name);
+    currentPlayer.health = healthUpdate;
+    currentPlayer.healing = "Moderate";
+    currentPlayer.reArm (ammo);
+    alert (currentPlayer.name + " has "+ currentPlayer.ammoCount + " in their arsonal.");
+}
+UpdatePlayer(Player1, 20, 1);
+UpdatePlayer(Player2, 60, 1000);
