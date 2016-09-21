f39c9779cbe3be627b56fae8619fcfd8ad6a73cb
Unknown <addisonbmoyer@gmail.com>
2016-09-21 11:19:57 -0600

    new object constructors

---
+++ Java script/in_class_assignments/objectConstructors.js
@@ -0,0 +1,15 @@
+
+
+function Player() {
+    var name;
+    var speed = 10;
+    var health = 100;
+}
+
+var Player1 = new Player();
+Player1.name = prompt("Player1 their leader");
+Player1.health = 8;
+
+var Player2 = new Player();
+Player2.name = prompt("Player2");
+Player2.speed =45;
