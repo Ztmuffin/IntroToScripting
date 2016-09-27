bb92cf9494403c8aa0a6569a86ebf1d9f386e27d
Unknown <addisonbmoyer@gmail.com>
2016-09-26 21:54:03 -0600

    test with yes

---
+++ Java script/in_class_assignments/switch_statements.js
@@ -0,0 +1,19 @@
+ 
+ var theExample;
+ var demonstrate = "yes".toLowerCase;
+
+ console.log (demonstrate);
+    switch (demonstrate) {
+        case "yes":
+            theExample = "So do I, they're great!";
+            break;
+        case "no":
+            theExample = "you should like them, they're great!";
+            break;
+        case "sometimes":
+            theExample = "Why not all the time? They're great";
+            break;
+        default:
+            theExample = "I'm sorry I do not understand what you have entered.";
+    }
+    alert(theExample);
