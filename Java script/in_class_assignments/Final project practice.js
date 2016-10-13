0071c1000f7598d6cbe9d164de9972d68a49b0a0
Unknown <addisonbmoyer@gmail.com>
2016-10-12 21:12:32 -0600

    need to look at making damage taken a function

--- Java script/in_class_assignments/Final project practice.js
+++ Java script/in_class_assignments/Final project practice.js
@@ -2,7 +2,12 @@
 
 var button = document.getElementById('name');
 var beginGame = function gameButton() {
-
+    
+updatePlayer (playerUser,10,"Strong", 60 );
+level1();
+level2();
+}
+button.addEventListener('click', beginGame);
     var player = function () {
         var name;
         var health = 10;
@@ -23,10 +28,10 @@ function updatePlayer (currentPlayer, healthUpdate, strengthUpdate, speedUpdate)
     currentPlayer.health = healthUpdate;
     currentPlayer.strength = strengthUpdate;
     currentPlayer.speed = speedUpdate;
-    
-        }
-updatePlayer (playerUser,10,"Strong", 60 );
+    }
 
+function level1() {
+    
 if (!playerUser.name) {
     alert ("please enter a name")
     window.location.reload();
@@ -35,10 +40,7 @@ if (!playerUser.name) {
     alert("Okay, "+ playerUser.name +" You begin with: "+ playerUser.health + " health.    "
 +playerUser.strength +" strength.   " + playerUser.speed + " Speed.     and " +playerUser.stealth +" Stealth"
  )
-
-
-
-    alert ("In a post apocaliptic world you're our only hope! "+ playerUser.name + " can we trust you to protect and save us? Pres OK if you accept the challenge");
+     alert ("In a post apocaliptic world you're our only hope! "+ playerUser.name + " can we trust you to protect and save us? Pres OK if you accept the challenge");
 
     var beginingSenarios = ["Starting on earth you need to find a weapon, but where do you go?",
     "Mars is a desolate planet, who knows how you ended up here, but you need to find a way to defend yourself. " + playerUser.name + " Lets go find a weapon."
@@ -47,9 +49,24 @@ if (!playerUser.name) {
         return Math.round(Math.random()*range);
     }
     alert(beginingSenarios [randomNumber (beginingSenarios.length -1)]);
+    }
+
+    alert("As you are looking for a weapon a Goblin approaches");
+       var RandomNumber1 = Math.round(Math.random()*2);
+       console.log(RandomNumber1);
+       if (RandomNumber1>1) {
+           updateEnimy(enimy1,4,10,75);
+           updatePlayer(playerUser,6,"Fair", 50);
+           alert("You took some damage from the Goblin! Though you killed them with a fork you found, they attacked you first.")
+           alert("Okay, "+ playerUser.name +" You're down to: "+ playerUser.health + " health.    "
+            +playerUser.strength +" strength.   " + playerUser.speed + " Speed.")
+        } else {
+           alert(playerUser.name +" you find a sharpened stake and luckily take out the Goblin that was chasing you down.")
+           updateEnimy(enimy1,0,0,0)
+           alert("Okay, "+ playerUser.name +" You're down to: "+ playerUser.health + " health.    "
+            +playerUser.strength +" strength.   " + playerUser.speed + " Speed.")
+       }
 }
-
-
 // make as objects: Enimies 3, weapons
 
 var enimy = function () {
@@ -69,35 +86,16 @@ var enimy = function () {
 
     }
 
-alert("As you are looking for a weapon a Goblin approaches");
-       var RandomNumber1 = Math.round(Math.random()*2);
-       console.log(RandomNumber1);
-       if (RandomNumber1>1) {
-           updateEnimy(enimy1,4,10,75);
-           updatePlayer(playerUser,6,"Fair", 50);
-           alert("You took some damage from the Goblin! Though you killed them with a fork you found, they attacked you first.")
-           alert("Okay, "+ playerUser.name +" You're down to: "+ playerUser.health + " health.    "
-            +playerUser.strength +" strength.   " + playerUser.speed + " Speed.")
-        } else {
-           alert(playerUser.name +" you find a sharpened stake and luckily take out the Goblin that was chasing you down.")
-           updateEnimy(enimy1,0,0,0)
-           alert("Okay, "+ playerUser.name +" You're down to: "+ playerUser.health + " health.    "
-            +playerUser.strength +" strength.   " + playerUser.speed + " Speed.")
-       }
-   } 
-button.addEventListener('click', beginGame);
-while (playerUser.health > 0) {
-   //do nothing 
-}else { alert("you loose!");
+while (playerUser.health < 0) { alert("you loose!");
     restartGame();
 }
 function level2 () {
    alert("you've found a weapon now, a rifle.  Another Goblin approaches, lets see if you know how to use that rifle");
-   
+
 }
 
 function restartGame () {
-    var q = confirm();
+    var q = confirm("Do you want to play again?");
     if (q== true) {
         window.location.reload();
     } else {
